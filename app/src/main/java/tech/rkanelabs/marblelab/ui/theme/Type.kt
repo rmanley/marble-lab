0f8d33a2dae91929c35f4aaf0eeabe77b7f007e0
Canaan Manley <canaanm1114@gmail.com>
2025-12-16 00:42:47 -0600

    Change font to match Marble Fall

--- app/src/main/java/tech/rkanelabs/marblelab/ui/theme/Type.kt
+++ app/src/main/java/tech/rkanelabs/marblelab/ui/theme/Type.kt
@@ -2,33 +2,26 @@ package tech.rkanelabs.marblelab.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import tech.rkanelabs.marblelab.R
 
-// Set of Material typography styles to start with
+val MedievalFontFamily = FontFamily(
+    Font(R.font.metamorphous_regular, FontWeight.Normal)
+)
+
+private val defaultTypography = Typography()
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    bodyLarge = defaultTypography.bodyLarge.copy(
+        fontFamily = MedievalFontFamily
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleLarge = defaultTypography.titleLarge.copy(
+        fontFamily = MedievalFontFamily
+    ),
+    labelLarge = defaultTypography.labelLarge.copy(
+        fontFamily = MedievalFontFamily
+    ),
 )
